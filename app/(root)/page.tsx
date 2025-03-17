28da4949c2e6378cf1246cd5d02f5af8a361987f
Andres Campuzano <campuxano88@gmail.com>
2025-03-16 19:32:19 -0500

    fetch from react component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,22 @@
-import { Hello } from "@/app/components/Hello";
-
-export default function Home() {
+export default async function Home() {
   console.log("Server component");
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  const data = await response.json();
 
   return (
-      <>
-        <h1 className="text-2xl">Server component</h1>
-        <Hello />
-      </>
+      <div>
+        <ul>
+          {data.map((album: { id: number, title: string }) => (
+              <li key={album.id}>
+                {album.title}
+              </li>
+          ))}
+        </ul>
+      </div>
   );
 }
