2925f7042c5c498eaae91f3e7f440b6d3f20147e
Andres Campuzano <campuxano88@gmail.com>
2025-03-16 21:13:13 -0500

    apir routes and server components

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="p-4">
-      <h1 className="text-3xl">Root layout</h1>
+      <h1 className="text-3xl">(root)</h1>
       {children}
     </div>
   );
---
+++ app/api/books/[id]/route.ts
@@ -0,0 +1,31 @@
+import books from "@/app/api/db"
+
+export async function PUT(
+    request: Request,
+    context: {
+        params: {
+            id: string
+        }
+    }) {
+        const book = await request.json()
+        const id = parseInt(context.params.id)
+
+        const index = books.findIndex((book) => book.id === id)
+        books[index] = book
+
+        return Response.json(book)
+}
+
+export async function DELETE(
+    _request: Request,
+    context: {
+        params: {
+            id: string
+        }
+    }) {
+        const id = parseInt(context.params.id)
+        const index = books.findIndex((book) => book.id === id)
+        books.splice(index, 1)
+
+        return Response.json({ id })
+}
